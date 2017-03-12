78c9d5f3bf40f2b4deecc10e031567df8c8a6e6a
J <james@j-macbookpro.local>
2017-03-12 19:29:06 -0400

    Writing to SQL

--- poker.py
+++ poker.py
@@ -1,5 +1,26 @@
 import random
 
+#!/usr/bin/python
+
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+
+print "Database version : %s " % data
+
+# disconnect from server
+db.close()
+
 cardSuits = ['hearts','clubs','spades','diamonds']
 cardValues = ['two','three','four','five','six','seven','eight','nine','ten','jack','queen','king','ace']
 deckOfCards = []
