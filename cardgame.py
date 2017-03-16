53e5d9de502d73cc0658fae4426d57f8c06a92b6
J <james@j-macbookpro.local>
2017-03-16 19:13:09 -0400

    Small error fix

--- cardgame.py
+++ cardgame.py
@@ -60,7 +60,7 @@ def check7TwoPair(hand):
     for each in cardValues:
         temp = value.count(each)
         if temp > 1:
-            pairs[each0] = True
+            pairs[each[0]] = True
     if len(pairs) > 1:
         return True
 def check4Flush(hand): #Working
