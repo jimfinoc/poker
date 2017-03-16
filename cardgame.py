17ad988d45752e05e7f19fcea17cfc7aa4b477a4
J <james@j-macbookpro.local>
2017-03-16 19:12:20 -0400

    Do we have a full house?

--- cardgame.py
+++ cardgame.py
@@ -145,7 +145,7 @@ def check9HighCard(hand):
     return True
 
 def check3FullHouse(hand):
-    if (check6ThreeOfAKind()) and (check7TwoPair()):
+    if (check6ThreeOfAKind(hand)) and (check7TwoPair(hand)):
         return True
     else:
         return False
@@ -408,63 +408,4 @@ while True:
     except:
         pass
     checkHands()
-
-    # for each in communityCards:
-    #     print each[1] + each[0]
-    #     part1 = 'INSERT INTO community_cards (Card) VALUES ("'
-    #     part2 = each[1] + each[0]
-    #     part3 = '");'
-    #     print part1 + part2 + part3
-
-    # Check for Winners
-
-
-    # 1. Royal flush
-    # A, K, Q, J, 10, all the same suit.
-    # A K Q J T
-        # royalFlush = False
-
-
-
-
-
-    # for each in gameCards:
-
-
-
-    # 2. Straight flush
-    # Five cards in a sequence, all in the same suit.
-    # 8 7 6 5 4
-    # 3. Four of a kind
-    # All four cards of the same rank.
-    # J J J J 7
-    #
-    # 4. Full house
-    # Three of a kind with a pair.
-    # T T T 9 9
-    #
-    # 5. Flush
-    # Any five cards of the same suit, but not in a sequence.
-    # 4 J 8 2 9
-    #
-    # 6. Straight
-    # Five cards in a sequence, but not of the same suit.
-    # 9 8 7 6 5
-    #
-    # 7. Three of a kind
-    # Three cards of the same rank.
-    # 7 7 7 K 3
-    #
-    # 8. Two pair
-    # Two different pairs.
-    # 4 4 3 3 Q
-    #
-    # 9. Pair
-    # Two cards of the same rank.
-    # A A 8 4 7
-    #
-    # 10. High Card
-    # When you haven't made any of the hands above, the highest card plays.
-    # In the example below, the jack plays as the highest card.
-    # 3 J 8 4 2
     wait = raw_input("That's the game. Press enter to continue.")
